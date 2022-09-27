647ec113a75c55ae7f6616b516648a1fad6dc1cf
Lucas Araujo <lcsaraujo@outlook.com>
2022-09-27 14:24:13 -0300

    Adicionando Vosk para reconhecimento de voz offline

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+""""
 import speech_recognition as sr
 
 r = sr.Recognizer()
@@ -6,3 +7,27 @@ with sr.Microphone() as source:
     audio = r.listen(source)
 
     print(r.recognize_google(audio, language='pt'))
+"""
+
+
+from vosk import Model, KaldiRecognizer
+import os
+import pyaudio
+
+model = Model('model')
+rec = KaldiRecognizer(model, 16000)
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
+
+while True:
+    data = stream.read(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
+
+print(rec.FinalResult())
