61f630c52eab5e0a02951a3c0e926ca3a7a231c0
Lucas Araujo <lcsaraujo@outlook.com>
2022-09-29 14:09:26 -0300

    Assistente utilizando o recurso de voz da Microsoft

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
-""""
-import speech_recognition as sr
+import webbrowser
 
-r = sr.Recognizer()
+from testassist import *
+def conversando_com_kira():
 
-with sr.Microphone() as source:
-    audio = r.listen(source)
+    if resultfala == "Olá, kira.":
+        synthesizer.speak_text_async("Olá Lucas, como você está ? O dia está lindo!")
 
-    print(r.recognize_google(audio, language='pt'))
-"""
-
-
-from vosk import Model, KaldiRecognizer
-import os
-import pyaudio
-
-model = Model('model')
-rec = KaldiRecognizer(model, 16000)
-
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
-stream.start_stream()
-
-while True:
-    data = stream.read(4000)
-    if len(data) == 0:
-        break
-    if rec.AcceptWaveform(data):
-        print(rec.Result())
+    elif resultfala == "Abra o Google.":
+        webbrowser.open("www.google.com")
+        synthesizer.speak_text_async("Pra já ! Abrindo o Google !")
     else:
-        print(rec.PartialResult())
+        synthesizer.speak_text_async("Desculpe, eu não entendi!")
 
-print(rec.FinalResult())
+conversando_com_kira()
